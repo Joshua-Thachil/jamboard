f72c096bd04d4af0c0916070abed9b2b952b2c04
Joshua Thachil <joshuathachil@gmail.com>
2024-10-15 08:51:28 +0530

    Added Toolbar on Lyrics Screen

--- lib/screens/LyricsScreen.dart
+++ lib/screens/LyricsScreen.dart
@@ -10,7 +10,6 @@ class LyricsScreen extends StatefulWidget {
 }
 
 class _LyricsScreenState extends State<LyricsScreen> {
-
   QuillController lyricsController = QuillController.basic();
 
   @override
@@ -19,26 +18,72 @@ class _LyricsScreenState extends State<LyricsScreen> {
         backgroundColor: Palette.bg,
         appBar: AppBar(
           backgroundColor: Colors.transparent,
-          title: Center(child: Padding(
+          title: Center(
+              child: Padding(
             padding: EdgeInsets.only(right: 30),
-            child: Text("Project Name", style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+            child: Text(
+              "Project Name",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
           )),
-          leading: IconButton(onPressed: null, icon: Icon(Icons.arrow_back, color: Colors.white,)),
+          leading: IconButton(
+              onPressed: null,
+              icon: Icon(
+                Icons.arrow_back,
+                color: Colors.white,
+              )),
         ),
         body: Padding(
           padding: const EdgeInsets.only(left: 15, top: 40, right: 15),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Expanded(
-                child: Container(child: QuillEditor.basic(
+                child: Container(
+                    child: QuillEditor.basic(
                   controller: lyricsController,
                   configurations: QuillEditorConfigurations(
-                    customStyles: DefaultStyles(
-                      paragraph: DefaultTextBlockStyle(TextStyle(color: Colors.white), HorizontalSpacing(5, 5), VerticalSpacing(2, 2), VerticalSpacing(1, 1), null)
-                    )
-                  ),
+                      customStyles: DefaultStyles(
+                          paragraph: DefaultTextBlockStyle(
+                              TextStyle(color: Colors.white, fontSize: 17),
+                              HorizontalSpacing(5, 5),
+                              VerticalSpacing(2, 2),
+                              VerticalSpacing(1, 1),
+                              null))),
                 )),
               ),
+              QuillToolbar.simple(
+                  controller: lyricsController,
+                  configurations: QuillSimpleToolbarConfigurations(
+                    dialogTheme: QuillDialogTheme(),
+                    showAlignmentButtons: false,
+                    showColorButton: false,
+                    showFontFamily: false,
+                    showDirection: false,
+                    showLineHeightButton: false,
+                    showSubscript: false,
+                    showSuperscript: false,
+                    showFontSize: false,
+                    showDividers: false,
+                    showSearchButton: false,
+                    showIndent: false,
+                    showQuote: false,
+                    showInlineCode: false,
+                    showHeaderStyle: false,
+                    showListBullets: false,
+                    showListNumbers: false,
+                    showLink: false,
+                    showCodeBlock: false,
+                    showListCheck: false,
+                    showUndo: false,
+                    showRedo: false,
+                    showClipboardCopy: false,
+                    showClipboardCut: false,
+                    showClipboardPaste: false,
+                    showClearFormat: false,
+                    toolbarIconCrossAlignment: WrapCrossAlignment.center
+                  ))
             ],
           ),
         ));
