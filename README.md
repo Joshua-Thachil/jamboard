68540b38f187c06b4cf0f27df03331a67bcaf7d1
aadzy <113158443+aadzy@users.noreply.github.com>
2024-10-17 12:46:21 +0530

    added UI from project screen

--- README.md
+++ README.md
@@ -44,6 +44,79 @@ runApp(MyApp());
 
 
 //methods
+
+Future<void> startRecording() async {
+// Get the app directory for storing temporary audio files
+Directory appDocDir = await getApplicationDocumentsDirectory();
+String filePath = '${appDocDir.path}/recording-${DateTime.now().millisecondsSinceEpoch}.aac';
+
+// Start recording and save the file locally
+await _recorder.openRecorder();
+await _recorder.startRecorder(toFile: filePath);
+print('Recording started...');
+}
+
+Future<String?> stopRecordingAndUpload() async {
+try {
+// Stop the recorder
+String? localFilePath = await _recorder.stopRecorder();
+await _recorder.closeRecorder();
+print('Recording stopped: $localFilePath');
+
+    if (localFilePath != null) {
+      // Get the file and upload to Supabase storage
+      File audioFile = File(localFilePath);
+      String fileName = 'audio_recordings/recording-${DateTime.now().millisecondsSinceEpoch}.aac';
+
+      // Upload the file to Supabase Storage
+      final response = await supabase.storage
+          .from('audio-recordings') // Use your Supabase storage bucket name
+          .upload(fileName, audioFile);
+
+      if (response.error == null) {
+        print('File uploaded successfully: ${response.data}');
+        return fileName; // Return the uploaded file's path
+      } else {
+        print('Error uploading file: ${response.error?.message}');
+        return null;
+      }
+    }
+} catch (e) {
+print('Error during recording or uploading: $e');
+return null;
+}
+return null;
+}
+
+
+Recording module methods:
+
+
+
+//dependencies
+dependencies:
+flutter_sound: ^9.0.0
+path_provider: ^2.0.0
+supabase_flutter: ^1.0.0
+permission_handler: ^10.0.0 # To request permission to record
+
+
+
+//supabase initialization
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void main() async {
+WidgetsFlutterBinding.ensureInitialized();
+await Supabase.initialize(
+url: 'https://your-project-url.supabase.co', // Your Supabase project URL
+anonKey: 'your-anon-key', // Your Supabase anon key
+);
+runApp(MyApp());
+}
+
+
+
+//methods
 FlutterSoundRecorder _recorder = FlutterSoundRecorder();
 final supabase = Supabase.instance.client;
 
--- lib/screens/RecordingScreen.dart
+++ lib/screens/RecordingScreen.dart
@@ -5,6 +5,8 @@ import 'package:record/record.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:io';
 
+import '../Style/Palette.dart';
+
 class RecordingScreen extends StatefulWidget {
   const RecordingScreen({super.key});
 
@@ -142,41 +144,211 @@ class _RecordingScreenState extends State<RecordingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Recording Screen"),
-      ),
-      body: Center(
+      backgroundColor: Palette.bg,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 26),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            if (_recordState == RecordState.record)
-              const Text(
-                "Recording...",
-                style: TextStyle(fontSize: 24),
-              ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: _recordState != RecordState.stop ? stopRecording : startRecording,
-              child: Text(_recordState != RecordState.stop ? "Stop" : "Record"),
+            const SizedBox(height: 80),
+            // Animated Title and Search Bar
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end, // Align the row's children to the right
+              children: [
+                // Conditionally display either title or search bar based on search activity
+                Expanded(
+                  child: Stack(
+                    // alignment: Alignment.centerRight, // Align the search bar to the right
+                    children: [
+                      // Search bar animation (grows from right to left)
+                      Container(
+                        padding: EdgeInsets.only(top: 8),
+                        child: AnimatedContainer(
+                          duration: Duration(milliseconds: 200),
+                          width: _isSearchActive
+                              ? MediaQuery.of(context).size.width - 90
+                              : 0, // Starts at 0 width and expands
+                          child: _isSearchActive
+                              ? TextField(
+                            onChanged: _filterProjects,
+                            controller: _searchController,
+                            autofocus: true,
+                            style: TextStyle(color: Colors.white),
+                            decoration: InputDecoration(
+                              hintText: 'Search project',
+                              hintStyle: TextStyle(color: Colors.white70),
+                              filled: true,
+                              fillColor: Palette.secondary_bg,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                                borderSide: BorderSide.none,
+                              ),
+                            ),
+                          )
+                              : SizedBox.shrink(), // Hide search bar when inactive
+                        ),
+                      ),
+                      // Title and subtitle animation (shown when search is inactive)
+                      AnimatedOpacity(
+                        opacity: _isSearchActive ? 0 : 1,
+                        duration: Duration(milliseconds: 100),
+                        child: Column(
+                          key: ValueKey("titleText"),
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              "Hello Arlene !",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 25,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(height: 8),
+                            const Text(
+                              "What are we working on today?",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                // Search/close icon
+                IconButton(
+                  icon: Icon(
+                    _isSearchActive ? Icons.close : Icons.search,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      if (_isSearchActive) {
+                        // Clear the search controller
+                        _searchController.clear();
+                        // Reset the filtered project list
+                        _filteredProjectList = _projectList; // Reset to show all projects
+                      }
+                      // Toggle the search state
+                      _isSearchActive = !_isSearchActive;
+                    });
+                  },
+                ),
+              ],
             ),
-            const SizedBox(height: 20),
-            if (_recordState == RecordState.stop && audioPath.isNotEmpty)
-              ElevatedButton(
-                onPressed: playRecording,
-                child: const Text("Play"),
+            const SizedBox(height: 30),
+
+            Expanded(
+              child: flag
+                  ? const Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // Image.asset(
+                    //   'assets/images/rb_2150544943.png', // Path to your no projects image
+                    //   width: 200, // Set width for the image
+                    //   height: 200, // Set height for the image
+                    // ),
+                    SizedBox(height: 20), // Space between image and text
+                    Text(
+                      'Wanna add a new project?', // Message when no projects
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+                  : ListView.builder(
+                itemCount: _filteredProjectList.length,
+                itemBuilder: (context, index) {
+                  final project = _filteredProjectList[index];
+
+                  return Dismissible(
+                    key: Key(project), // Unique key for each project
+                    background: Container(
+                      color: Colors.red, // Background color when swiping
+                      alignment: Alignment.centerLeft,
+                      padding: EdgeInsets.only(left: 20),
+                      child: Icon(Icons.delete, color: Colors.white), // Delete icon
+                    ),
+                    onDismissed: (direction) {
+                      setState(() async {
+                        // Remove the project from the original list
+                        _projectList.remove(project);
+                        //await Supabase.instance.client.from('Projects').delete().eq('title', project);
+
+                        // Remove from the filtered list
+                        _filteredProjectList.removeAt(index);
+                      });
+
+                      // // Optionally, show a snackbar
+                      // ScaffoldMessenger.of(context).showSnackBar(
+                      //   SnackBar(content: Text("$project dismissed")),
+                      // );
+                    },
+                    child: Container(
+                      width: double.infinity,
+                      margin: EdgeInsets.only(bottom: 12),
+                      child: TextButton.icon(
+                        style: ButtonStyle(
+                          padding: WidgetStateProperty.all(EdgeInsets.symmetric(horizontal: 20, vertical: 25)),
+                          backgroundColor: WidgetStateProperty.all(Palette.secondary_bg),
+                          shape: WidgetStateProperty.all(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                          alignment: Alignment.centerLeft,
+                        ),
+                        onPressed: () {
+                          // Define your onPressed action here
+                          //Navigator.push(context, MaterialPageRoute(builder: (context) => LyricsScreen()));
+                        },
+                        icon: Icon(
+                          Icons.music_note_outlined,
+                          color: Palette.primary,
+                          size: 35,
+                        ),
+                        label: Text(
+                          project,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.normal,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                },
               ),
-            const SizedBox(height: 40),
-            if (_amplitude != null)
-              Column(
-                children: [
-                  Text('Current amplitude: ${_amplitude?.current ?? 0.0}'),
-                  Text('Max amplitude: ${_amplitude?.max ?? 0.0}'),
-                ],
-              ),
-            const SizedBox(height: 20),
-            if (_recordState != RecordState.stop) _buildTimer(),
+            ),
+
           ],
+        ),
+      ),
+      bottomNavigationBar: Container(
+        margin: EdgeInsets.only(bottom: 40),
+        child: BottomAppBar(
+          color: Palette.bg,
+          child: Center(
+              child: GestureDetector(
+                onTap: () {
+                  _showInputDialog(context);
+                },
+                child: Icon(
+                  Icons.add_circle_rounded,
+                  color: Palette.primary,
+                  size: 80,
+                ),
+              )
+          ),
         ),
       ),
     );
